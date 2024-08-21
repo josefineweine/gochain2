9daa363368a8064057a3f9ca0ace9c4463f6e177
josefineweine <114985858+josefineweine@users.noreply.github.com>
2024-08-21 10:31:46 +0200

    push backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './Router'
+
+function App() {
+
+  return (
+    <>
+		<RouterProvider router={router}/>
+	  </>
+  )
+}
+
+export default App
